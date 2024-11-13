40e1bda9984b5fc26101a1a0f03f1c1c4e85d63e
Anthony Bruno <anthonyqy333@gmail.com>
2024-11-13 10:28:01 -0300

    Projeto Full Stack

--- funcionario_uml/routes/funcionario.js
+++ funcionario_uml/routes/funcionario.js
@@ -26,15 +26,16 @@ router.post('/store',async(req,res)=>{
 
    
 //2ª rota - Exibir pagina raiz de funcionario
-router.get('/', (req,res)=>{
-    res.send("<h1> Página inicial de Funcionário</h1>")
+router.get('/show', (req,res)=>{
+    res.render('funcionario/index')
 })
 
 //3ª rota - consultar  dados da tabela
-router.get('/show',async(req,res)=>{
-    let resultado = await funcionario.findAll()
+router.get('/',async(req,res)=>{
+    let resultado = await funcionario.findAll({include:departamento})
     if (resultado){
         console.log(resultado)
+        res.render('funcionario/index', {dados:resultado})
     }
     else{
         console.log("Não foi possível exibir os dados")
@@ -47,13 +48,23 @@ router.get('/destroy/:id', async(req,res)=>{
     const resultado = await funcionario.destroy({
         where:{
             id:req.params.id //estamos recebendo o id via parâmetro, que está sendo passado na rota, no caso é o :id que estamos recebendo.
-
         }
     })
+    res.redirect('/')
 })
 
 //5ª rota - exibir formulario de cadastro
-router.get("/create",(req,res)=>{
-    res.render('funcionarios/addFuncionario')
+router.get("/create", async(req,res)=>{
+    let resultado = await departamento.findAll()
+    if(resultado){
+        console.log(resultado)
+        res.render('funcionario/addFuncionario', {dados:resultado})
+
+    }
+    else{
+        console.log("Não foi possível carregar nenhum dado")
+        res.redirect('/')
+    }
+    
 })
 module.exports = router
